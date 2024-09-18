06ae07eb26c79f1a57b85378edd8e935117443ef
Alperen sungur <97175131+AlperenSungurMIS@users.noreply.github.com>
2024-09-18 16:16:58 +0300

    Intern Day 1

--- Day1/Day1Encaps.py
+++ Day1/Day1Encaps.py
@@ -2,7 +2,7 @@ class urun:
     def __init__(self):
         self.__fiyat = 1000
     def fiyatyaz(self):
-        print("ürün fiyatı: {}",self.__fiyat)
+        print("ürün fiyatı:",self.__fiyat)
 
 
 u = urun()
